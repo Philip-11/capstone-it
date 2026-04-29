98df4795e821ffafdac3ce3d864cdf210d91b963
Philip-11 <magsaysayphil@gmail.com>
2026-04-29 21:40:32 +0800

    added function to attempt model

--- app/Models/Attempt.php
+++ app/Models/Attempt.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attempt extends Model
 {
-    //
+    protected $fillable = ['user_id', 'quiz_id', 'score', 'total_questions', 'completed_at'];
+
+    //To link back to the student
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    //To link back to the quiz they took
+    public function quiz(): BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
 }

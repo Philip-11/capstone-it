a260f5f62d53eb05493269069a85b164ae2ab055
Philip-11 <magsaysayphil@gmail.com>
2026-05-01 23:50:31 +0800

    created auth controller for login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class AuthController extends Controller
+{
+    public function showLogin()
+    {
+        return Inertia::render('Auth/login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'], 
+        ]);
+
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            if (Auth::user()->role === 'teacher'){
+                return redirect()->intended('/teacher/lessons');
+            }
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+}

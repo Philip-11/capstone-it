f44a03aebab6894448e7cad80df555473c673034
Philip-11 <magsaysayphil@gmail.com>
2026-05-01 23:54:41 +0800

    added new routes for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
 
 Route::middleware(['auth', 'verified'])->group(function () {
 

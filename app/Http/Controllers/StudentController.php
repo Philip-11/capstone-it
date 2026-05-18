a27d52378948980fdcb65de123b37586146293f7
Philip-11 <magsaysayphil@gmail.com>
2026-05-18 15:58:13 +0800

    added logic for showing and submition of quiz

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Attempt;
+use App\Models\Quiz;
 use App\Models\Subject;
 use Auth;
 use Illuminate\Http\Request;
@@ -42,6 +44,54 @@ class StudentController extends Controller
 
     public function showLesson($id){
 
+    }
+
+    public function showQuiz(Quiz $quiz)
+    {
+        $quiz->load(['questions' => function($query){
+            $query->select('id', 'quiz_id', 'question_text', 'question_type', 'options', 'points');
+        }]);
+
+        return Inertia::render('Student/QuizView', [
+            'quiz' => $quiz
+        ]);
+    }
+
+    public function strclean($string)
+    {
+        return strtolower(trim($string));
+    }
+
+    public function submitQuiz(Request $request, Quiz $quiz)
+    {
+        //'answers' is an array from vue
+        $studentAnswers = $request->input('answers', []);
+
+        $totalQuestions = $quiz->questions()->count();
+        $score = 0;
+
+        //take all the questions with answers for checking
+        $questions = $quiz->questions();
+
+        foreach ($questions as $question){
+            $studentAnswer = $studentAnswers[$question->id] ?? null;
+            //compare the student's answer to correct answer
+            if (strclean($studentAnswer) === strclean($question->correct_answer)) {
+                $score += $question->points;
+            }
+        }
+
+        $attempt = Attempt::create([
+            'user_id' => Auth::id(),
+            'quiz_id' => $quiz->id,
+            'score' => $score,
+            'total_questions' => $totalQuestions,
+            'student_answers' => $studentAnswers,
+            'completed_at' => now(),
+        ]);
+
+        return redirect()->route('student.subjects.show', $quiz->lesson->subject_id)
+            ->with('message', 'Quiz submitted!');
     }
 
     public function takeQuiz($id){

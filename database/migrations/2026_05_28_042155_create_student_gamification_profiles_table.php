bfe332d10ce276ed16ee55b1173b847be1ef7eb4
Philip-11 <magsaysayphil@gmail.com>
2026-05-28 13:07:38 +0800

    created table for gamification

---
+++ database/migrations/2026_05_28_042155_create_student_gamification_profiles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_gamification_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade');
+            $table->integer('xp')->default(0);
+            $table->integer('level')->default(1);
+            $table->integer('streak')->default(0);
+            $table->integer('streak')->default(0);
+            $table->date('last_activity_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_gamification_profiles');
+    }
+};

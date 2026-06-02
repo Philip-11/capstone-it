24d37b57701e789416e77311aca7f3fa4d2d9baa
Philip-11 <magsaysayphil@gmail.com>
2026-06-02 11:52:58 +0800

    added name on route login

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\StudentAssignmentController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/login', [AuthController::class, 'showLogin']);
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
